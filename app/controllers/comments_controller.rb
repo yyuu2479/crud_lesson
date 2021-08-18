b2276e562de42602ebf61e1abb03c503493ab06f
yyuu2479 <yyuu2479@gmail.com>
2021-08-18 11:24:51 +0000

    CRUD

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,47 +1,55 @@
 class CommentsController < ApplicationController
   before_action :authenticate_user!, only:[:index, :show, :edit]
-  
+  before_action :ensure_correct_comment, only:[:edit, :destroy]
+
   def top
   end
-  
+
   def index
     @comments = Comment.page(params[:page]).reverse_order
     @comment = Comment.new
   end
-  
+
   def show
     @comment = Comment.find(params[:id])
   end
-  
+
   def edit
     @comment = Comment.find(params[:id])
   end
-  
+
   def create
     @comment = Comment.new(comment_params)
     @comment.user_id = current_user.id
     @comment.save
     redirect_to comments_path
   end
-  
+
   def destroy
     comment = Comment.find(params[:id])
     comment.destroy
     redirect_to comments_path
   end
-  
+
   def update
     comment = Comment.find(params[:id])
     comment.update(comment_params)
     redirect_to comment_path(comment)
   end
-  
-  
-  
+
+
+
   private
-  
+
   def comment_params
     params.require(:comment).permit(:title, :body, :image)
   end
-  
+
+  def ensure_correct_comment
+    @Comment = Comment.find(params[:id])
+    unless @Comment.user == current_user
+      redirect_to comments_path
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :ensure_correct_user, only:[:edit]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -18,4 +20,12 @@ class UsersController < ApplicationController
   def user_name_params
     params.require(:user).permit(:name, :profile_image)
   end
+  
+  def ensure_correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+      redirect_to user_path(current_user)
+    end
+  end
+  
 end
