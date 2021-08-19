0f141e9c3387e1a67cb1b4a32f6537e1588956ae
yyuu2479 <yyuu2479@gmail.com>
2021-08-19 05:09:34 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only:[:edit]
-  
+
   def show
     @user = User.find(params[:id])
+    @comments = @user.comments.page(params[:page]).per('2').reverse_order
+    @favorites = @user.favorites
   end
 
   def edit
@@ -14,18 +16,18 @@ class UsersController < ApplicationController
     @user.update(user_name_params)
     redirect_to user_path(@user)
   end
-  
+
   private
-  
+
   def user_name_params
     params.require(:user).permit(:name, :profile_image)
   end
-  
+
   def ensure_correct_user
     @user = User.find(params[:id])
     unless @user == current_user
       redirect_to user_path(current_user)
     end
   end
-  
+
 end
