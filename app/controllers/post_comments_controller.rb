c6ccc9ec51402e64d3439fc80c61debd1748aabe
yyuu2479 <yyuu2479@gmail.com>
2021-08-21 06:50:08 +0000

    crud

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,5 +1,5 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
     comment = Comment.find(params[:comment_id])
     @post_comment = PostComment.new(post_comment_params)
@@ -8,15 +8,15 @@ class PostCommentsController < ApplicationController
   @post_comment.save
     redirect_back fallback_location: root_path
   end
-  
+
   def destroy
     comment = Comment.find(params[:comment_id])
     PostComment.find_by(user_id: current_user.id, comment_id: comment.id).destroy
     redirect_back fallback_location: root_path
   end
-  
+
   private
-  
+
   def post_comment_params
     params.require(:post_comment).permit(:post_comment)
   end
