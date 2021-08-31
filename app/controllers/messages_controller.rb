9f148ce421074f1f649e7d44e34f3dd49aa313a7
yyuu2479 <yyuu2479@gmail.com>
2021-08-31 14:50:20 +0000

    chat

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,18 @@
 class MessagesController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
       @message = Message.create(message_params)
-      redirect_to room_path(@message.room_id)
-    else
-      flash[:alert] = "メッセージが送信できませんでした"
+      @room = @message.room
+      @messages = @room.messages
     end
   end
-  
+
   private
-  
+
   def message_params
     params.require(:message).permit(:message, :user_id, :room_id).merge(user_id: current_user.id)
   end
-  
+
 end
--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,27 +1,26 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
-  
+
   def create
     @room = Room.create
-    @entry1 = Entry.create(user_id: current_user, room_id: @room.id)
+    @entry1 = Entry.create(user_id: current_user.id, room_id: @room.id)
     @entry2 = Entry.create(params_user_entry)
     redirect_to room_path(@room.id)
   end
 
   def show
     @room = Room.find(params[:id])
-    
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
       @messages = @room.messages
       @message = Message.new
-      @entry = @room.entries
+      @entrys = @room.entries
       @another_entry = @entrys.where.not(user_id: current_user.id).first
     end
-    
+
   end
-  
+
   private
-  def params_uset_entry
+  def params_user_entry
     params.require(:entry).permit(:user_id, :room_id).merge(room_id: @room.id)
   end
 end
